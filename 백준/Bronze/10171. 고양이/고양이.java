5fbe9875958cb16863e1eaafc0176d31091c4d01
JinWoo Kim <136250818+jinvvoo@users.noreply.github.com>
2023-08-16 13:39:30 +0900

    [Bronze V] Title: 고양이, Time: 124 ms, Memory: 14004 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
         System.out.println(" \\(__)|");
-        System.out.println("");
-
     }
 }
+
+
