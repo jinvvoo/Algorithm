33c4d092c08ea196645490db2611bfb16f6c610b
JinWoo Kim <136250818+jinvvoo@users.noreply.github.com>
2023-08-17 10:30:51 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for(int i = 1; i<10; i++) {
+            System.out.println(a + " * " + i + " = " + a*i);
+        }
+
+
+
+//        System.out.println(a + " * 1" + " = " + a*1);
+//        System.out.println(a + " * 2" + " = " + a*2);
+//        System.out.println(a + " * 3" + " = " + a*3);
+//        System.out.println(a + " * 4" + " = " + a*4);
+//        System.out.println(a + " * 5" + " = " + a*5);
+//        System.out.println(a + " * 6" + " = " + a*6);
+//        System.out.println(a + " * 7" + " = " + a*7);
+//        System.out.println(a + " * 8" + " = " + a*8);
+//        System.out.println(a + " * 9" + " = " + a*9);
+
+    }
+}
